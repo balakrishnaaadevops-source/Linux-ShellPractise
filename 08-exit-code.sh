6767c62f0ff9f61ffdbc5a9d87bbe5671e410a07
Balakrishna <balakrishna.wla@.com>
2026-05-23 21:49:23 +0530

    first specical variables

--- 08-exit-code.sh
+++ 08-exit-code.sh
@@ -1,17 +1,19 @@
 #!bin/bash
-ls /tmp
+LOGS_DIR="/home/ec2-user/logs"
+LOGS_FILE="$LOGS_DIR/$#.log
+ls /tmp &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
  echo "SUCCESS"
 else
  echo "FAILURE"
 fi
-ls /fakedir
+ls /fakedir &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
 echo "SUCCESS"
 else
 echo "FAILURE"
 fi
-ping -c1 google.com
+ping -c1 google.com &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
 echo "SUCCESS"
 else
