ba268cbe16206388f955490f164dd6d846236415
Balakrishna <balakrishna.wla@.com>
2026-05-23 21:56:37 +0530

    first specical variables

--- 08-exit-code.sh
+++ 08-exit-code.sh
@@ -1,22 +1,17 @@
 #!bin/bash
 LOGS_DIR="/home/ec2-user/logs"
 LOGS_FILE="$LOGS_DIR/$0.log"
-ls /tmp &>> $LOGS_FILE
+VALIDATE()
+{
 if [ $? -eq 0 ]; then
  echo "SUCCESS"
 else
  echo "FAILURE"
 fi
+}
+ls /tmp &>> $LOGS_FILE
+VALIDATE()
 ls /fakedir &>> $LOGS_FILE
-if [ $? -eq 0 ]; then
-echo "SUCCESS"
-else
-echo "FAILURE"
-fi
+VALIDATE()
 ping -c1 google.com &>> $LOGS_FILE
-if [ $? -eq 0 ]; then
-echo "SUCCESS"
-else
-echo "FAILURE"
-fi
-
+VALIDATE()
