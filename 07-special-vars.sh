7a33126b53d20da99e7a0fc9d47edacc7252a220
Balakrishna <balakrishna.wla@.com>
2026-05-23 16:14:25 +0530

    first specical variables

---
+++ 07-special-vars.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+# Print who is running the script
+# Print the current directory
+# Print the home directory
+# Print the PID of the script
+# Run sleep 10 in the background and print its PID
+# Print a random number between 1–100
+# At the end, print how many seconds the script took to run
+
+echo "Who is running : $whoami"
+echo "Cuurent directory : $pwd"
+echo "Home directory : $HOME"
+echo "current script PID : $$"
+sleep 5 &
+echo "backgroup of PID : $!"
+echo "Randam number $RANDAM"
+echo "seconds of the script took $SECONDS"
