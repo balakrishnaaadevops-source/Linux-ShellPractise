0de14b1c5d64a7e8c71d05bf7de5668448f27c8f
Balakrishna <balakrishna.wla@.com>
2026-05-23 16:36:10 +0530

    first specical variables

--- 08-exit-code.sh
+++ 08-exit-code.sh
@@ -1,12 +1,8 @@
 #!bin/bash
-# Runs ls /tmp and checks if it succeeded — print SUCCESS or FAILURE
-# Runs ls /fakedir and checks — print SUCCESS or FAILURE
-# Runs ping -c1 google.com and checks — print Host reachable or Host not #reachable
-# At the end, exit the script with code 0 if all commands succeeded, or 1 if #any failed
-
 ls /tmp
 if [$? -ne 0 ]; then
 echo "SUCCESS"
+exit 1
 fi
 echo "FAILURE"
 
