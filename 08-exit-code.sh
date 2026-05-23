0c85f806b623f5524f5b70ee799f1c66d012397e
Balakrishna <balakrishna.wla@.com>
2026-05-23 21:37:48 +0530

    first specical variables

--- 08-exit-code.sh
+++ 08-exit-code.sh
@@ -5,5 +5,16 @@ if [ $? -eq 0 ]; then
 else
  echo "FAILURE"
 fi
-
+ls /fakedir
+if [ $? -eq 0 ]; then
+echo "SUCCESS"
+else
+echo "FAILURE"
+fi
+ping -c1 google.com
+if [ $? -eq 0 ]; then
+echo "SUCCESS"'
+else
+echo "FAILURE"
+fi
 
