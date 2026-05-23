a8e48f179f88f576286b8ec3e4cb5c79143c175a
Balakrishna <balakrishna.wla@.com>
2026-05-23 13:46:23 +0530

    first specical variables

--- special_varaibles.sh
+++ special_varaibles.sh
@@ -4,3 +4,8 @@ echo "Number of variables passed to script: $#"
 echo "First variable : $1"
 echo "Script name: $0"
 echo "PID of the current script $$"
+sleep 3 &
+echo "PID of the background command $!"
+echo "line number $LINENO"
+echo "script executed $SECONDS seconds"
+echo "Random number $RANDOM"
