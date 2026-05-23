6212ce06e6dfd2b290f019c314556b9fa70f7bcd
Balakrishna <balakrishna.wla@.com>
2026-05-23 14:30:03 +0530

    first specical variables

--- 03-condition.sh
+++ 03-condition.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 NUMBER=$1
-if [$NUMBER -ge 20 ]; then
+if [ $NUMBER -ge 20 ]; then
  echo "given number $NUMBER is grater than or equal 20"
  else
   echo "given number $NUMBER is less than 20"
