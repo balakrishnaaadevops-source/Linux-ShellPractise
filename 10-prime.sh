f9ac04fa7000ec7eaf6f323109df61e5f7cfe9cf
Balakrishna <balakrishna.wla@.com>
2026-05-24 15:19:13 +0530

    first specical variables

--- 10-prime.sh
+++ 10-prime.sh
@@ -5,3 +5,13 @@ echo "$num :is not a prime number"
 else 
 echo "$num : is a prime number"
 fi
+for (( i=2; i<num; i++ ))
+do
+ if(( num%i -eq 0  ))
+ then
+ echo "$num is not a prime"
+ exit
+ fi
+ done
+ echo "$num is a prime number"
+ 
