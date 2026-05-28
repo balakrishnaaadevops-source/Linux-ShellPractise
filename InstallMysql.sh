7725657e5283ece895cc26956def01c356a24501
Balakrishna <balakrishna.wla@.com>
2026-05-28 11:32:51 +0530

    first specical variables

--- InstallMysql.sh
+++ InstallMysql.sh
@@ -9,11 +9,28 @@ if [ $USER -ne 0 ]; then
  echo "Run Script $0 with root user or sudo user "
  exit 1
 fi
+validate()
+{
+ if [ $2 -ne 0 ]; then
+ echo "Installing $1 FAILED"
+ exit 1
+else 
+ echo "Installing $1 SUCCESS"
+ 
+fi
+}
 dnf list installed mysql
 if [ $? -eq 0 ]; then
- echo "mysql installed alreday"
-exit 1
- else 
- echo "Installing mysql"
- dnf install mysql -y
+echo "MYSQL installed SKIPPING ..."
+else
+echo "Installing MYSQL"
+dnf install mysql -y
+validate $?
+fi
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+echo "Nginx Installed SKIPPING ..."
+else
+echo "Installing Nginx"
+dnf install nginx -y
 fi
