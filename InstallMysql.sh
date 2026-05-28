d9a469d6c5ea5fec769a46484f1e2c509a7189e2
Balakrishna <balakrishna.wla@.com>
2026-05-28 10:44:47 +0530

    first specical variables

--- InstallMysql.sh
+++ InstallMysql.sh
@@ -4,8 +4,9 @@
 # first check the user has executed with root or sudo permissions
 # if yes then list out of the mysql alreday installed or not
 # if not then install mysql 
-if [ $? -e 0 ]
+if [ $? -e 0 ]; then
  echo "Installing MYSQL"
  exit 0
+else
  echo "Run script : $0 with root user"
 fi
